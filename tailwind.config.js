a6dab6648ec6199e4293d4a01e1179ffd18aa4bf
nader-web <94446474+nader-web@users.noreply.github.com>
2024-09-30 19:40:24 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // CONTENT FOLDERS IS CSS folder & root folder
+    "./*.css",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
